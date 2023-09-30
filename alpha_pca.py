c041fac6ec2c814cde2533dd7c494af5994f55a8
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2023-09-30 04:15:12 +0300

    SYNV

--- alpha_pca.py
+++ alpha_pca.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import statsmodel.api as sm
 
 # Download Data
 tech_tickers = ['AAPL', 'MSFT', 'AMZN', 'GOOG']
@@ -26,9 +27,9 @@ pca.fit(returns)
 # Plot principal components
 fig, ax = plt.subplots(figsize=(12, 6), dpi=100)
 # enumerate creates a tuple of index and pairwise ellements from the principal components that have been zipped into a preceeding tuple
-# i identifies the stock with it's new relative position in the new principal axi/principal components
+# i identifies the stock with it's new relative position in the new principal axii/principal components
 # the scatter plot is a pairwise plot of each stock's variance along a principal component ie x, y are the stock's coordinates in the PCA space
-# using pca.components_ consider investigating the explained variance associated with each principal component
+# using pca.components_ consider investigating the explained variance/EV associated with each principal component
 for i, (x, y) in enumerate(zip(pca.components_[0], pca.components_[1])): 
     ax.scatter(x, y, marker='o', s=100)
     ax.annotate(returns.columns[i], xy=(x, y), xytext=(x+0.04, y+0.06),
@@ -41,7 +42,7 @@ ax.set_ylabel('Principal Component 2')
 ax.set_title('Principal Components')
 plt.show()
 
-# Calculate alpha factors
+# Calculate alpha factors. This is a projection of each stock's daily returns onto the PCA space by each eigenvector
 alpha_factors = pd.DataFrame()
 for i in range(3):
     alpha_factors[f'alpha_{i+1}'] = returns.dot(pca.components_[i])
@@ -49,9 +50,23 @@ for i in range(3):
 # Add alpha factors to returns data
 alpha_returns = pd.concat([alpha_factors, returns], axis=1)
 
-# Plot scatter matrix of alpha factors
+# Plot scatter matrix of alpha factors: which represents the pairwise relationship between each alpha factor 
+# and also a univariate plot along the diagonal of the scatteroplot matrix
 sns.set(style='ticks', palette='viridis')
 g = sns.pairplot(alpha_factors)
 g.fig.set_size_inches(10,10)
 plt.show()
+
+# calculate alpha of each stock by regressing (OLSr) it's returns against each alpha factor
+# alpha factors are the projection of returns against the principal components/eigenvectors of the covariance matrix
+alpha = pd.DataFrame()
+for i in range(n_components):
+    model = sm.OLS(alpha_returns.iloc[:, 3+i], alpha_returns.iloc[:, :3])
+    results = model.fit()
+    alpha[f'alpha_{i+1}'] = results.params
+alpha_mean = alpha.mean()
+alpha_std = alpha.std()
+print(f'Mean alpha: \n{alpha_mean}')
+print('='*50)
+print(f'Std alpha: \n{alpha_std}')
 
